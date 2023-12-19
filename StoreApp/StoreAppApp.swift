3f52b2bc790feebdb3af7ab38857f471030bb626
MariamKipiani <145837678+MariamKipiani@users.noreply.github.com>
2023-12-19 09:25:41 +0400

    Initial Commit

---
+++ StoreApp/StoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreAppApp.swift
+//  StoreApp
+//
+//  Created by user on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct StoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
